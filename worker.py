4a85c8d267e96328217a119f29953b7b9476862d
Posma Janius Sianturi <posmajaniuss@gmail.com>
2024-11-29 12:58:16 +0700

    Initial commit

---
+++ worker.py
@@ -0,0 +1,23 @@
+from llama_index.core.tools import FunctionTool
+from llama_index.llms.openai import OpenAI
+from dotenv import load_dotenv
+import os
+from llama_index.core.agent import (
+    StructuredPlannerAgent,
+    FunctionCallingAgentWorker,
+)
+
+# load file .env
+load_dotenv()
+
+
+def multiply(a: int, b: int) -> int:
+ """Multiply two integers and returns the result integer"""
+ return a * b
+
+multiply_tool = FunctionTool.from_defaults(fn=multiply)
+llm = OpenAI(model="gpt-4o-mini")
+worker = FunctionCallingAgentWorker.from_tools([multiply_tool], llm=llm, verbose=True)
+worker_agent = StructuredPlannerAgent(worker, [multiply_tool], verbose=True)
+
+worker_agent.chat("Solve the equation x = 123 * (x + 2y + 3)")
